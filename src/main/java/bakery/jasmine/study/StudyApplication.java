0c759fa56aa68448643d5943aa16d90efcf7d2af
demo.lim <demo.lim@kakaocorp.com>
2020-08-28 22:31:25 +0900

    first commit

---
+++ src/main/java/bakery/jasmine/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package bakery.jasmine.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
