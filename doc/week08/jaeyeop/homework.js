3860e414d6003619cd633a4de33a69edc83a07a2
정재엽 <jeongjaegyeop@jeongjaeyeob-ui-MacBookPro.local>
2020-11-21 20:33:36 +0900

    week08_homework

---
+++ doc/week08/jaeyeop/homework.js
@@ -0,0 +1,17 @@
+var a = 20;
+  async function foo(){
+    function sleep(ms) {
+        return new Promise(
+          resolve => setTimeout(resolve, ms)
+        );
+    }
+    await sleep(1000);
+    a = a + 1;
+    console.log("foo " + a);
+  }
+  function bar(){
+     a = a * 2;
+     console.log("bar " + a);
+  }
+  foo();
+  bar();
